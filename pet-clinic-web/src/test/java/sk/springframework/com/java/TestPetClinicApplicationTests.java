2b7e6a278321d1fe77d5f2930778cdea6ab9357a
che6kor <shilpakala.chennabasappa@in.bosch.com>
2019-10-05 16:37:43 +0530

    Fixed and Close the Issue #3 add maven release.

---
+++ pet-clinic-web/src/test/java/sk/springframework/com/java/TestPetClinicApplicationTests.java
@@ -0,0 +1,16 @@
+package sk.springframework.com.java;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TestPetClinicApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
