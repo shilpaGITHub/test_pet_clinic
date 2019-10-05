85deea4e80ca342154ef82ef7f6f90f46ba940fa
che6kor <shilpakala.chennabasappa@in.bosch.com>
2019-10-05 09:57:41 +0530

    Initial Project Add

---
+++ src/main/java/sk/springframework/com/java/test_pet_clinic/TestPetClinicApplication.java
@@ -0,0 +1,13 @@
+package sk.springframework.com.java.test_pet_clinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestPetClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestPetClinicApplication.class, args);
+    }
+
+}
