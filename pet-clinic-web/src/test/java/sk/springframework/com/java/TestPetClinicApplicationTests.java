0059dea7d206e23eee940e29237c2ae62dba4b8f
che6kor <shilpakala.chennabasappa@in.bosch.com>
2019-10-07 15:18:27 +0530

    close Issue NO 08

--- pet-clinic-web/src/test/java/sk/springframework/com/java/TestPetClinicApplicationTests.java
+++ pet-clinic-web/src/test/java/sk/springframework/com/java/TestPetClinicApplicationTests.java
@@ -1,9 +1,13 @@
 package sk.springframework.com.java;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
 
-/*@RunWith(SpringRunner.class)
-@SpringBootTest*/
+@RunWith(SpringRunner.class)
+/*@SpringBootTest*/
+@ContextConfiguration
 public class TestPetClinicApplicationTests {
 
     @Test
